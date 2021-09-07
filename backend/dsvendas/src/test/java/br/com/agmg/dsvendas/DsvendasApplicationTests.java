a80e673de6258335346a18438be630eec239bb67
Alexandre Guerra <agmguerra@gmail.com>
2021-09-07 14:03:38 -0300

    Project created

---
+++ backend/dsvendas/src/test/java/br/com/agmg/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agmg.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
