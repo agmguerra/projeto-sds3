27d18a2836494545734c61eb63dfc55a16f089fb
Alexandre Guerra <agmguerra@gmail.com>
2021-09-08 22:56:00 -0300

    ajustes no diretório backend

---
+++ backend/DsVendas/src/main/java/br/com/agmg/dsvendas/DsVendasApplication.java
@@ -0,0 +1,13 @@
+package br.com.agmg.dsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsVendasApplication.class, args);
+	}
+
+}
---
+++ backend/DsVendas/src/test/java/br/com/agmg/dsvendas/DsVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agmg.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
