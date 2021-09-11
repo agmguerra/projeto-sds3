9790a2747a6b0190825b1ead07e4dab54efad6c8
Alexandre Guerra <agmguerra@gmail.com>
2021-09-11 11:18:29 -0300

    Home, navaigation

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -11,7 +11,7 @@ const Routes = () => {
                 <Route path="/" exact>
                     <Home />
                 </Route>
-                <Route path="/">
+                <Route path="/dashboard">
                     <Dashboard />
                 </Route>
             </Switch>
